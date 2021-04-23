fc6f82543ccb24f4a2a5dfd2a6e9690e21d3e02c
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-04-24 02:29:57 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "tree.h"
+#include "for_all.h"
+
+
+
+int main() {
+    Tree *tree = NULL;
+    tree = new_tree(tree);
+    dialog(tree);
+    printf("That's all. Bye!\n");
+    delete_tree(tree);
+    return 0;
+}
