1f6e06927e0140c86a1d45c72cf8cd0786b08193
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-04-24 02:30:05 +0300

    Create for_all.h

---
+++ for_all.h
@@ -0,0 +1,16 @@
+//
+// Created by yura on 23.04.2021.
+//
+
+#ifndef LAB4A_SEM2_FOR_ALL_H
+#define LAB4A_SEM2_FOR_ALL_H
+
+#include "tree.h"
+
+void dialog(Tree*);
+void scan_int(int*);
+char *scan_string(char *);
+Info *scan_info(Info*);
+
+
+#endif //LAB4A_SEM2_FOR_ALL_H
